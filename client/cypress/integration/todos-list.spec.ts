f4cc2978e681c0b3cc8c31cbe368242369472407
Thomas <dahlg136@morris.umn.edu>
2021-02-16 22:45:47 -0600

    Added e2e testing
    Added tests to make sure all of the now added todo website functions work and can access the /todos url and view individual profiles with the correct information in them. Co-authored-by: Josh Quist quist127@morris.umn.edu

---
+++ client/cypress/integration/todos-list.spec.ts
@@ -0,0 +1,53 @@
+import { TodosListPage } from '../support/todos-list.po';
+
+const page = new TodosListPage();
+
+describe('Todos list', () => {
+
+  beforeEach(() => {
+    page.navigateTo();
+  });
+
+  it('Should have the correct title', () => {
+    page.getTodosTitle().should('have.text', 'Todos');
+  });
+
+
+  it('Should change the view', () => {
+    // Choose the view type "List"
+    page.changeView('list');
+
+    // We should not see any cards
+    // There should be list items
+    page.getTodosCards().should('not.exist');
+    page.getTodosListItems().should('exist');
+
+    // Choose the view type "Card"
+    page.changeView('card');
+
+    // There should be cards
+    // We should not see any list items
+    page.getTodosCards().should('exist');
+    page.getTodosListItems().should('not.exist');
+  });
+
+  it('Should click view profile on a todos and go to the right URL', () => {
+    page.getTodosCards().first().then((card) => {
+      const firstTodosOwner = card.find('.todos-card-owner').text();
+      const firstTodosCategory = card.find('.todos-card-category').text();
+
+      // When the view profile button on the first todos card is clicked, the URL should have a valid mongo ID
+      page.clickViewProfile(page.getTodosCards().first());
+
+      // The URL should contain '/todos/' (note the ending slash) and '/todos/' should be followed by a mongo ID
+      cy.url()
+        .should('contain', '/todos/')
+        .should('match', /.*\/todos\/[0-9a-fA-F]{24}$/);
+
+      // On this profile page we were sent to, the owner and category should be correct
+      cy.get('.todos-card-owner').first().should('have.text', firstTodosOwner);
+      cy.get('.todos-card-category').first().should('have.text', firstTodosCategory);
+    });
+   });
+
+});
